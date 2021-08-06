4846638ba486ebee83eb23cab8ab76bb7e908be4
a-xavier <43228621+a-xavier@users.noreply.github.com>
2021-08-06 12:56:52 +1000

    Fixed search amazon

--- src/amazon.py
+++ src/amazon.py
@@ -35,50 +35,61 @@ def amazon_search(search_string):
     if response: # RESPONSE OK
         text_response = response.text
         soup = BeautifulSoup(text_response, 'html.parser')
+        #print(soup)
+
 
         # GET ALL OBJECTS WITH CLASS=ARTICLE
           # last ITEM IS NOT A REAL SEARCH RESULT
-        list_of_products = soup.find_all('div',  src=lambda x: x and 'price' in x)
+        list_of_products = soup.find_all('div', lambda L: L and L.startswith('s-result-item'))
 
-        list_of_products = [x for x in list_of_products if "result-item" in x["class"][0]]
+        #print(len(list_of_products))
+
+        #list_of_products = [x for x in list_of_products if "result-item" in x["class"][0]]
 
         for article in list_of_products:
+            #print(article)
 
             if article.h2: # IF THERE IS A "DID yOU MEAN TYPO"
 
                 comic_title = article.h2.text
 
-                comic_url = "https://www.amazon.com.au" + article.find("a", class_ = "a-link-normal s-no-outline")["href"]
+                #print(comic_title)
+                try:
+                    comic_url = "https://www.amazon.com.au" + article.find("a", class_ = "a-link-normal s-no-outline")["href"]
 
-                # SPLITTING FOR WHEN IT HAS REDUCED PRICE TOO
+                    #print(comic_url)
 
-                comic_price = article.find("span", class_ = "a-offscreen").text # IF NOT THERE IT'S UNAVAILABLE
+                    # SPLITTING FOR WHEN IT HAS REDUCED PRICE TOO
 
-                comic_price = float(comic_price.replace("$",""))
+                    comic_price = article.find("span", class_ = "a-offscreen").text # IF NOT THERE IT'S UNAVAILABLE
 
-                comic = {"title": comic_title,
-                         "url": comic_url,
-                         "price": comic_price,
-                         "shop":shop,
-                         "availability": "In Stock"}
+                    comic_price = float(comic_price.replace("$",""))
 
-                # DO SOME MATCHING
-                # CRUDE = TAKE ALL 3+ LETTER WORDS IN SEARCH AND SEE IF THEY ARE IN COMIC TITLE
-                list_word_search = [x.lower() for x in search_list if len(x)> 2]
+                    comic = {"title": comic_title,
+                             "url": comic_url,
+                             "price": comic_price,
+                             "shop":shop,
+                             "availability": "In Stock"}
 
-                list_word_title = comic_title.split(" ")
-                list_word_title = [x.lower() for x in list_word_title if len(x)> 2]
-                # REMOVE COLONS, coma
-                list_word_title = [x.replace(":","") for x in list_word_title]
-                list_word_title = [x.replace(",","") for x in list_word_title]
+                    # DO SOME MATCHING
+                    # CRUDE = TAKE ALL 3+ LETTER WORDS IN SEARCH AND SEE IF THEY ARE IN COMIC TITLE
+                    list_word_search = [x.lower() for x in search_list if len(x)> 2]
 
-                #print(list_word_search)
-                #print(list_word_title)
-                #print(all(item in list_word_title for item in list_word_search))
-                # IF ALL WORDS FROM SEARCH ARE IN TITLE: RETURN
+                    list_word_title = comic_title.split(" ")
+                    list_word_title = [x.lower() for x in list_word_title if len(x)> 2]
+                    # REMOVE COLONS, coma
+                    list_word_title = [x.replace(":","") for x in list_word_title]
+                    list_word_title = [x.replace(",","") for x in list_word_title]
 
-                if all(item in list_word_title for item in list_word_search):
-                    result_holder.append(comic)
+                    #print(list_word_search)
+                    #print(list_word_title)
+                    #print(all(item in list_word_title for item in list_word_search))
+                    # IF ALL WORDS FROM SEARCH ARE IN TITLE: RETURN
+
+                    if all(item in list_word_title for item in list_word_search):
+                        result_holder.append(comic)
+                except TypeError: # IF NOT A REAL ARTICLE
+                    pass
     # ON FAILURE
     else:
         print('An error has occurred searching {}.'.format(shop))
